7a34496f1b11f105ed3008c3cbe855e952cf85f2
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-02 07:02:59 -0800

    added in code from class. needs to be cleaned up a bit

--- class-04-templates/lecture/demo-code/handlebars/scripts/neighborhood.js
+++ class-04-templates/lecture/demo-code/handlebars/scripts/neighborhood.js
@@ -1,19 +1,17 @@
 var neighborhoods = [];
-
 function Neighborhood (opts) {
   for (key in opts) {
     this[key] = opts[key];
   }
 };
-
 Neighborhood.prototype.toHtml = function() {
-
+    var $source = $(‘#neighborhood-template’).html();
+    var templateRender = Handlebars.compile(‘$source’);
+    return templateRender(this);
 };
-
 neighborhoodDataSet.forEach(function(neighborhoodObject) {
   neighborhoods.push(new Neighborhood(neighborhoodObject));
 });
-
 neighborhoods.forEach(function(ourNewNeighborhoodObject){
   $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
 });
